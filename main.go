45189acdf5ac6441e2488722f012334086d4dbe8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-28 21:10:12 +0530

    Using HandleFunc

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(f.Message))
 }
 
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Bar Called"))
+}
+
 func main() {
 	fmt.Println("Hello World")
 	http.Handle("/foo", &fooHandler{Message: "Hello"})
+	http.HandleFunc("/bar", barHandler)
 	http.ListenAndServe(":5000", nil)
 }

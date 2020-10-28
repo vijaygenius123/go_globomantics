6e849492873f2494ea542ebc6fd524e1da0d140a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-28 21:08:30 +0530

    Added Basic handler

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type fooHandler struct {
+	Message string
+}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(f.Message))
+}
 
 func main() {
 	fmt.Println("Hello World")
+	http.Handle("/foo", &fooHandler{Message: "Hello"})
+	http.ListenAndServe(":5000", nil)
 }
